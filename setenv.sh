fa1c6cf19ac94064252b1f5549a0d8aaee1050fc
snowdog.app <dog@snowdog.app>
2024-04-06 16:21:34 +0900

    modified: setenv.sh # for Anaconda

--- setenv.sh
+++ setenv.sh
@@ -1,7 +1,7 @@
 # How to use: `source setenv.sh`
 
 export LIBTORCH_USE_PYTORCH=1
-export LD_LIBRARY_PATH=$(python -m site --user-site)/torch/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$(python -c "import site; print(site.getsitepackages()[0])")/torch/lib:$LD_LIBRARY_PATH
 
 echo LIBTORCH_USE_PYTORCH=$LIBTORCH_USE_PYTORCH
 echo LD_LIBRARY_PATH=$LD_LIBRARY_PATH
